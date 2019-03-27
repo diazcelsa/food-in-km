132faeca31e6eaf8bfcf95d0fa9c6afdf4d9dd9a
LBrinkmann <com@levin-brinkmann.de>
2019-03-27 18:05:38 +0100

    Sum for status bar.

--- foodkm-frontend/src/container/StatusBar.jsx
+++ foodkm-frontend/src/container/StatusBar.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
-const StatusBar = ({totalPrice=70,totalKM=1300,onClick}) => (
+const StatusBar = ({totalPrice=70,totalKM=1300,onCheckout}) => (
     <div id="status-bar">
       <div className="status-bar-title">Totales</div>
       <div className="status-bar-price">{totalPrice}€</div>
       <div className="status-bar-km">{totalKM}km</div>
-      <button id="add-to-cart-button" onClick={onClick}>Ver carrito</button>
+      <button id="add-to-cart-button" onClick={onCheckout}>Ver carrito</button>
     </div>
 )
 
-// const BasketComponent = ({basket}) => {
-//     const totalPrice =
-// }
+const BasketComponent = ({basket,onCheckout}) => {
+    const totalPrice = _.sum(basket.map(({price}) => price));
+    const totalKM = _.sum(basket.map(({distance}) => distance));
+    return (
+        <StatusBar totalPrice={totalPrice} totalKM={totalKM} onCheckout={onCheckout}/>
+    )
+};
 
-// const StatusBarContainer = connect(
-//     (state, ownProps) => ({
-//         list: state.productList
-//     }),
-//     (dispatch, ownProps) => ({
-//         onChange: (value) => dispatch(a.searchProducts(value))
-//     })
-// )(StatusBar)
 
-export default StatusBar
+const BasketContainer = connect(
+    (state, ownProps) => ({
+        basket: state.basket
+    }),
+    (dispatch, ownProps) => ({
+        onCheckout: (value) => dispatch(a.searchProducts(value))
+    })
+)(BasketComponent)
+
+export default BasketContainer
