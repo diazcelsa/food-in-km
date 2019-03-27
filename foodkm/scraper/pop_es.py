b91d0f516438a88ba94d29a7f44d622989351f22
LBrinkmann <com@levin-brinkmann.de>
2019-03-27 01:51:52 +0100

    Fix elastisearch populator.

--- foodkm/scraper/pop_es.py
+++ foodkm/scraper/pop_es.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 import os
 from elasticsearch import Elasticsearch
 from pkg_resources import resource_filename
@@ -6,8 +7,9 @@ from pkg_resources import resource_filename
 es = Elasticsearch(
     [os.environ['FOODKM_ES_HOST']],
     scheme="http",
-    port=os.environ['FOODKM_ES_PORT'],
+    port=os.environ['FOODKM_ES_PORT']
 )
+
 
 def load_json():
     product_file = resource_filename(__name__, 'df/products.json')
@@ -15,12 +17,20 @@ def load_json():
         return json.load(f)
 
 
-def import_es():
-    products = load_json()
-    for p in products:
-        res = es.index(index="food_in_km", doc_type='_doc', body=p)
-        print(res)
+def parse_location(sr):
+    return {'lat': sr.lat, 'lon': sr.longs}
 
+
+def import_es():
+    products = pd.read_csv('data/sample_clean_vector.csv')
+    products['location'] = products[['lat', 'longs']].apply(parse_location, axis=1)
+    products = products.drop(['lat', 'longs'], axis=1)
+    for index, row in products.iterrows():
+        body = row.to_dict()
+        body = {k: v for k, v in body.items() if not pd.isna(v)}
+        print(body)
+        res = es.index(index="food_in_km", doc_type='_doc', body=body)
+        print(res)
 
 if __name__ == "__main__":
     import_es()
