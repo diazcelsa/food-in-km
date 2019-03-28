b00897ca646e59e2251a99f59aca55ff3408b55b
LBrinkmann <com@levin-brinkmann.de>
2019-03-28 16:29:24 +0100

    Change google api,

--- foodkm-frontend/src/config.jsx
+++ foodkm-frontend/src/config.jsx
@@ -1,5 +1,5 @@
 export var BACKEND_URL;
 
 if (typeof BACKEND_URL == 'undefined') {
-    BACKEND_URL = 'http://0.0.0.0:5000';
+    BACKEND_URL = 'http://127.0.0.1:5000';
 }
--- foodkm-frontend/src/container/ProductList.jsx
+++ foodkm-frontend/src/container/ProductList.jsx
@@ -4,10 +4,10 @@ import * as a from '../actions';
 import { connect } from 'react-redux';
 
 
-const ProductListItem = ({category_child1, category_child2, additives, allergens,
+const ProductListItem = ({category_child1, category_child2, additives, allergens, address,
     product_brand,product_name,price,distance,idx,onClick}) => (
     <div className="product-list-item">
-      <div className="product-list-item-name">{category_child1} {category_child2} {product_brand} {product_name} #additives{additives} allergens{allergens}</div>
+      <div className="product-list-item-name">{category_child1} {category_child2} {product_brand} {product_name} #additives{additives} allergens{allergens} {address}</div>
       <div className="product-list-item-price">{(price ? price.toFixed(2) + "€" : null)}</div>
       <div className="product-list-item-km">{(distance ? Math.round(distance) + "km" : null)}</div>
       <button className="add-to-cart-button" onClick={onClick}>+</button>
